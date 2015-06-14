077ee1cda8dea38111e04dd11d5a153f7f3e5604
normeow <rgdar@yandex.ru>
2015-06-14 21:53:43 +0300

    1. replace image from mipmap to drawable 2. edit list_view_item

--- app/src/main/java/example/normeow/sunshine/DetailActivity.java
+++ app/src/main/java/example/normeow/sunshine/DetailActivity.java
@@ -38,18 +38,13 @@ public class DetailActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        /*int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-        if (id == R.id.action_share){
-            Intent intent = new Intent();
-            return true;
-        }*/
-
         return super.onOptionsItemSelected(item);
     }
 
